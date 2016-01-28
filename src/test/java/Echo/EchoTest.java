103396e9adc44e2aa23a547ed702a8efcfa4af37
Laurent Claessens <moky.math@gmail.com>
2016-01-28 06:15:32 +0100

    Here are the test files.

---
+++ src/test/java/Echo/EchoTest.java
@@ -0,0 +1,45 @@
+/*
+Copyright 2016 Laurent Claessens
+contact : moky.math@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//*/
+
+// This is testing the Echo implementation of my actor system.
+//
+// To be tested 
+//   - if the serie number of the first one is -1 or 0 or 1.
+//   - if the ordering is correct.
+//   - try to send a Message of type other than EchoText
+
+import actors.ActorRef;
+
+import actors.impl.Echo.EchoActor;
+import actors.impl.Echo.EchoActorRef;
+import actors.impl.Echo.EchoActorSystem;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EchoTest {
+
+    @Test
+    public void Numbering()
+    {
+        EchoActorSystem system = new EchoActorSystem();
+        ActorRef a1 = system.actorOf(EchoActor.class);
+        ActorRef a2 = system.actorOf(EchoActor.class);
+    }
+}
---
+++ src/test/java/Echo/MailBoxTest.java
@@ -0,0 +1,54 @@
+/*
+Copyright 2016 Laurent Claessens
+contact : moky.math@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//*/
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import actors.ActorRef;
+import actors.MailBox;
+import actors.impl.Echo.EchoText;
+import actors.impl.Echo.EchoActor;
+import actors.impl.Echo.EchoActorSystem;
+
+public class MailBoxTest {
+
+    private MailBox mail_box;
+    private EchoText m2;
+
+    @Test
+    public void MailBoxFIFO()
+    {
+        MailBox<EchoText> mail_box=new MailBox<EchoText>();
+        EchoActorSystem system = new EchoActorSystem();
+        ActorRef a1 = system.actorOf(EchoActor.class);
+        ActorRef a2 = system.actorOf(EchoActor.class);
+
+        mail_box.add( new EchoText(a1,a2,3)  );
+        mail_box.add( new EchoText(a1,a2,13)  );
+        mail_box.add( new EchoText(a1,a2,23)  );
+
+        EchoText m2=mail_box.poll();
+        Assert.assertTrue(m2.getData()==3);
+        mail_box.add( new EchoText(a1,a2,33)  );
+        m2=mail_box.poll();
+        Assert.assertTrue(m2.getData()==13);
+        Assert.assertTrue(mail_box.size()==2);
+    }
+}
