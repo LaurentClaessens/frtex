2b3a0c457be435f2d20f48a5d89f0699fe601460
Laurent Claessens <moky.math@gmail.com>
2016-01-31 15:33:46 +0100

    Adding the unipd testing files.

---
+++ src/test/java/ActorIT.java
@@ -0,0 +1,114 @@
+/**
+ * The MIT License (MIT)
+ * <p/>
+ * Copyright (c) 2015 Riccardo Cardin
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * <p/>
+ * Please, insert description here.
+ *
+ * @author Riccardo Cardin
+ * @version 1.0
+ * @since 1.0
+ */
+
+/**
+ * Please, insert description here.
+ *
+ * @author Riccardo Cardin
+ * @version 1.0
+ * @since 1.0
+ */
+package actors;
+
+import actors.utils.ActorSystemFactory;
+import actors.utils.actors.TrivialActor;
+import actors.utils.actors.counter.CounterActor;
+import actors.utils.actors.ping.pong.PingPongActor;
+import actors.utils.actors.StoreActor;
+import actors.utils.messages.StoreMessage;
+import actors.utils.messages.counter.Increment;
+import actors.utils.messages.ping.pong.PingMessage;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Integration test suite on actor features.
+ *
+ * @author Riccardo Cardin
+ * @version 1.0
+ * @since 1.0
+ */
+public class ActorIT {
+
+    private ActorSystem system;
+
+    /**
+     * Initializes the {@code system} with a concrete implementation before each test.
+     */
+    @Before
+    public void init() {
+        this.system = ActorSystemFactory.buildActorSystem();
+    }
+
+    @Test
+    public void shouldBeAbleToSendAMessage() throws InterruptedException {
+        TestActorRef ref = new TestActorRef(system.actorOf(StoreActor.class));
+        StoreActor actor = (StoreActor) ref.getUnderlyingActor(system);
+        // Send a string to the actor
+        ref.send(new StoreMessage("Hello World"), ref);
+        // Wait that the message is processed
+        Thread.sleep(2000);
+        // Verify that the message is been processed
+        Assert.assertEquals("The message has to be received by the actor", "Hello World", actor.getData());
+    }
+
+    @Test
+    public void shouldBeAbleToRespondToAMessage() throws InterruptedException {
+        TestActorRef pingRef = new TestActorRef(system.actorOf(PingPongActor.class));
+        TestActorRef pongRef = new TestActorRef(system.actorOf(PingPongActor.class));
+
+        pongRef.send(new PingMessage(), pingRef);
+
+        Thread.sleep(2000);
+
+        PingPongActor pingActor = (PingPongActor) pingRef.getUnderlyingActor(system);
+        PingPongActor pongActor = (PingPongActor) pongRef.getUnderlyingActor(system);
+
+        Assert.assertEquals("A ping actor has received a ping message", "Ping",
+                pingActor.getLastMessage().getMessage());
+        Assert.assertEquals("A pong actor has received back a pong message", "Pong",
+                pongActor.getLastMessage().getMessage());
+    }
+
+    @Test
+    public void shouldNotLooseAnyMessage() throws InterruptedException {
+        TestActorRef counter = new TestActorRef(system.actorOf(CounterActor.class));
+        for (int i = 0; i < 200; i++) {
+            TestActorRef adder = new TestActorRef(system.actorOf(TrivialActor.class));
+            adder.send(new Increment(), counter);
+        }
+
+        Thread.sleep(2000);
+
+        Assert.assertEquals("A counter that was incremented 1000 times should be equal to 1000",
+                200, ((CounterActor) counter.getUnderlyingActor(system)).getCounter());
+    }
+}
---
+++ src/test/java/ActorSystemTest.java
@@ -0,0 +1,108 @@
+/**
+ * The MIT License (MIT)
+ * <p/>
+ * Copyright (c) 2015 Riccardo Cardin
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * <p/>
+ * Please, insert description here.
+ *
+ * @author Riccardo Cardin
+ * @version 1.0
+ * @since 1.0
+ */
+
+/**
+ * Please, insert description here.
+ *
+ * @author Riccardo Cardin
+ * @version 1.0
+ * @since 1.0
+ */
+package actors;
+
+import actors.exceptions.NoSuchActorException;
+import actors.utils.ActorSystemFactory;
+import actors.utils.actors.TrivialActor;
+import actors.utils.messages.TrivialMessage;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests features of an actors' system.
+ *
+ * @author Riccardo Cardin
+ * @version 1.0
+ * @since 1.0
+ */
+public class ActorSystemTest {
+
+    private ActorSystem system;
+
+    /**
+     * Initializes the {@code system} with a concrete implementation before each test.
+     */
+    @Before
+    public void init() {
+        system = ActorSystemFactory.buildActorSystem();
+    }
+
+    @Test
+    public void shouldCreateAnActorRefWithActorOfTest() {
+        ActorRef ref = system.actorOf(TrivialActor.class);
+        Assert.assertNotNull("A reference was created and it is not null", ref);
+    }
+
+    @Test
+    public void shouldCreateAnActorRefOfWithActorModeLocalTest() {
+        ActorRef ref = system.actorOf(TrivialActor.class, ActorSystem.ActorMode.LOCAL);
+        Assert.assertNotNull("A reference to a local actor was created and it is not null", ref);
+    }
+
+    /**
+     * It is not requested to implement remote mode for actors anymore. So, an attempt to create a remote
+     * actor should rise an {@link IllegalArgumentException}
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldCreateAnActorRefOfWithActorModeRemoteTest() {
+        system.actorOf(TrivialActor.class, ActorSystem.ActorMode.REMOTE);
+    }
+
+    @Test
+    public void shouldBeAbleToCreateMoreThanOneActor() {
+        ActorRef ref1 = system.actorOf(TrivialActor.class);
+        ActorRef ref2 = system.actorOf(TrivialActor.class);
+        Assert.assertNotEquals("Two references that points to the same actor implementation are not equal", ref1, ref2);
+    }
+
+    @Test(expected = NoSuchActorException.class)
+    public void shouldStopAnActorAndThisCouldNotBeAbleToReceiveNewMessages() {
+        ActorRef ref1 = system.actorOf(TrivialActor.class);
+        system.stop(ref1);
+        ref1.send(new TrivialMessage(), ref1);
+    }
+
+    @Test(expected = NoSuchActorException.class)
+    public void shouldStopAnActorAndThisCouldNotStoppedASecondTime() {
+        ActorRef ref1 = system.actorOf(TrivialActor.class);
+        system.stop(ref1);
+        system.stop(ref1);
+    }
+}
