5d2f23cedb9c0133f98ea9899f4fb6fd0968c534
Laurent Claessens <moky.math@gmail.com>
2016-06-16 05:15:30 +0200

    cosmetic

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -18,9 +18,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package actors;
 
-/**
- * A message that can be sent among actors
- *
- */
-
 public interface Message { };
